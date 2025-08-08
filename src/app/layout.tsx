1e0e5e01540dcde77d525ea479b09f9d07b810d1
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-08 12:56:19 -0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { montserrat } from "@/components/ui/fonts";
+import "../styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,10 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.className}  antialiased`}
       >
+        <h1>sadsa</h1>
         {children}
       </body>
     </html>
---
+++ src/components/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Montserrat} from "next/font/google";
+
+
+export const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
