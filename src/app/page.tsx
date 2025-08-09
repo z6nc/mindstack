128ca33e2609f392af9cd216570cf17bc8d705d5
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-09 13:18:49 -0500

    Refactor Home component to use InformacionComando and adjust command display logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-import { TerminalComando } from "@/components/ui/terminal/terminalbtn";
+import { InformacionComando } from "@/components/ui/Comando/infoComando";
 import { Hero } from "@/components/header/Hero";
 import { ListaComandos } from "@/data/listaComandos";
 import { ButtonCustom } from "@/components/ui/button/ButtonCustom";
-import { Suspense } from "react";
+// import { Suspense } from "react";
 export default function Home() {
   return (
-    <main className="max-w-6xl mx-auto  min-h-screen text-center flex flex-col justify-center items-center py-9 md:py-6 ">
+    <main className="max-w-6xl mx-auto  min-h-screen text-center flex flex-col  items-center py-12  ">
       <Hero />
       <section className="flex flex-col gap-y-9 py-4 ">
-        {ListaComandos.slice(0, 2).map((comando) => (
-          <Suspense key={comando.NombreComando} fallback={<div>Loading...</div>}>
-            <TerminalComando comando={comando} />
-          </Suspense>
+        {ListaComandos.slice(0, 1).map((comando) => (
+          // <Suspense key={comando.NombreComando} fallback={<div>Loading...</div>}>
+            <InformacionComando key={comando.NombreComando} comando={comando} />
+          // </Suspense>
         ))}
       </section>
-      <ButtonCustom href="/" texto="Ver todos los comandos"/>
+      <ButtonCustom href="/ListaComandos" texto="Ver todos los comandos" />
     </main>
   );
 }
