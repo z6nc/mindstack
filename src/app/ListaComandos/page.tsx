ff622edf6ba98d786ab909ee6bd3148fc07da727
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-09 13:18:56 -0500

    Refactor ListaComandos component to integrate search and filter functionalities

--- src/app/ListaComandos/page.tsx
+++ src/app/ListaComandos/page.tsx
@@ -1,17 +1,24 @@
-import { CategoriaComando } from "@/types/listaComandos.types";
-export default function Custom404() {
-    return (
-        <main className="max-w-6xl mx-auto  min-h-screen text-center flex flex-col  items-center py-9 md:py-6 ">
-            <article className="flex flex-wrap w-full  gap-4 justify-center items-center">
-                {
-                    Object.values(CategoriaComando).map((categoria) => (
-                        <button key={categoria} className="py-2 border border-black p-2 rounded-lg">
-                            <span className="text-sm ">{categoria}</span>
-                        </button>
-                    ))
-                }
-            </article>
+import { FiltroCategoria } from "@/components/ui/listacategoria/listaCategoria";
+import { BuscadorComandos } from "@/components/ui/search/search";
+import { FiltroComando } from "@/components/filtroComandos/filtroComando";
+export default async function ListaComandos({
+    searchParams,
+}: {
+    searchParams: Promise<{ query?: string , category?:string}>
+}) {
+    const params = await searchParams;
+    const busquedaUsuario = params.query ;
+    const busquedacategoria = params.category ;
 
+
+    return (
+        <main className="max-w-6xl mx-auto min-h-screen text-center flex flex-col items-center py-9 md:py-6">
+            <header className="flex flex-col  justify-center items-center gap-4">
+                <BuscadorComandos />
+                <FiltroCategoria />
+                <FiltroComando inputTexto={busquedaUsuario as string} filtroCategoria={busquedacategoria as string} />
+            </header>
         </main>
     );
 }
+
