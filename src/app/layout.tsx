134f1f416b6afb54d39e62cd03e002bc20587f12
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-08 20:32:27 -0500

    Refactor layout component to include Navbar and Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { montserrat } from "@/components/ui/fonts";
 import "../styles/globals.css";
-
+import { Navbar } from "@/components/navbar/Navbar";
+import { Footer } from "@/components/footer/footer";
 
 
 export const metadata: Metadata = {
@@ -19,8 +20,9 @@ export default function RootLayout({
       <body
         className={`${montserrat.className}  antialiased`}
       >
-        <h1>sadsa</h1>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
