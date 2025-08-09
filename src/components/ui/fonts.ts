e6ef1dfd7369db7fea6a2ad03f50552cb133fc52
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-08 20:33:36 -0500

    Fix formatting in fonts.ts by adding space after import statement

--- src/components/ui/fonts.ts
+++ src/components/ui/fonts.ts
@@ -1,7 +1,8 @@
-import { Montserrat} from "next/font/google";
+import { Montserrat } from "next/font/google";
 
 
 export const montserrat = Montserrat({
   variable: "--font-montserrat",
   subsets: ["latin"],
 });
+
