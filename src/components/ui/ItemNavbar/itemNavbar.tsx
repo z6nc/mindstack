78e7d6e714c7e3ec6ff9c34e28157a73ebce5e82
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-08 20:33:43 -0500

    Add ItemNavbar component for navigation with active link styling

---
+++ src/components/ui/ItemNavbar/itemNavbar.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import Link from "next/link"
+interface PropsNavItem {
+    titulo: string;
+    url: string;
+    icono: React.ReactNode;
+}
+import { usePathname } from "next/navigation";
+export const ItemNavbar = ({ url, icono }: PropsNavItem) => {
+    const pathname = usePathname();
+    const isActive = pathname === url;
+
+    return (
+
+        <Link
+            href={url}
+            className={`flex items-center space-x-2 text-black border border-gray-200 p-2 rounded-lg ${isActive ? "bg-[#00d1ff] text-white" : ""}`}
+        >
+            {icono}
+        </Link>
+
+
+    )
+}
