5b100a4bef5a50692db9ed6a10fb983548d17998
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-08 20:34:09 -0500

    Add ListaItemNavbar component for navigation with icons

---
+++ src/data/itemNavbar.tsx
@@ -0,0 +1,24 @@
+import { HomeIcon ,GithubIcon , Code ,House} from "lucide-react";
+interface PropsNavItem {
+  titulo: string;
+  url: string;
+  icono: React.ReactNode;
+}
+
+export const ListaItemNavbar: PropsNavItem[] = [
+  {
+    titulo: "Home",
+    url: "/",
+    icono :<House className="w-5 h-5 md:w-6 md:h-6" />
+  },
+    {
+    titulo: "Comandos",
+    url: "/ListaComandos",
+    icono :<Code className="w-5 h-5 md:w-6 md:h-6"/>
+  },
+  {
+    titulo: "Github",
+    url: "/ListadeIA",
+    icono :<GithubIcon className="w-5 h-5 md:w-6 md:h-6"/>
+  },
+];
