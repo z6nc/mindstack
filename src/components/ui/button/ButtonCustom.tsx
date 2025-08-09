7d2fa49c769bff7d8f4ce3d1cecfb82cd9b55623
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-09 13:19:18 -0500

    Update ButtonCustom component to change button background color to 'bg-react'

--- src/components/ui/button/ButtonCustom.tsx
+++ src/components/ui/button/ButtonCustom.tsx
@@ -3,7 +3,7 @@ import { ShinyButton } from "@/components/magicui/shiny-button";
 export const ButtonCustom = ({ href  , texto } :{ href: string; texto: string; }) => {
     return (
         <Link href={href}>
-            <ShinyButton className=" border-none bg-[#00d1ff]  ">{texto}</ShinyButton>
+            <ShinyButton className=" border-none bg-react  ">{texto}</ShinyButton>
         </Link>
     )
 }
