88fc6baf23e4b50f0668c03acecab924ef5bc087
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-13 17:57:33 -0500

    Add new utility and graphics libraries to ListaComandos for enhanced functionality

--- src/data/listaComandos.ts
+++ src/data/listaComandos.ts
@@ -201,7 +201,107 @@ export const ListaComandos: PropsListaComandos[] = [
       bun: "bun create next-app@latest my-app",
       yarn: "yarn create next-app my-app"
     }
-  }
+  },
+  {
+    NombreComando: "Zod",
+    descripcion:
+      "Zod es una biblioteca de validación de esquemas para TypeScript. Permite definir y validar estructuras de datos de manera sencilla y declarativa.",
+    tamaño: "Variable",
+    categoria: CategoriaComando.UTILIDADES,
+    documentacion: "https://zod.dev/",
+    codigo: {
+      npm: "npm install zod",
+      pnpm: "pnpm add zod",
+      bun: "bun add zod",
+      yarn: "yarn add zod",
+      deno: "deno add npm:zod"
+    }
+  },
+  {
+    NombreComando: "Day.js",
+    descripcion:
+      "Day.js es una biblioteca de manipulación de fechas para JavaScript. Es ligera y ofrece una API similar a Moment.js.",
+    tamaño: "2KB",
+    categoria: CategoriaComando.UTILIDADES,
+    documentacion: "https://day.js.org/docs/en/installation/node-js",
+    codigo: {
+      npm: "npm install dayjs",
+      pnpm: "pnpm add dayjs",
+      yarn: "yarn add dayjs",
+    }
+  },
+  {
+    NombreComando: "Zustand",
+    descripcion:
+      "Zustand es una biblioteca de gestión de estado para React. Es ligera y fácil de usar, y permite crear tiendas de estado de manera sencilla.",
+    tamaño: "Variable",
+    categoria: CategoriaComando.ESTADO_DATOS,
+    documentacion: "https://zustand.docs.pmnd.rs/getting-started/introduction",
+    codigo: {
+      npm: "npm install zustand",
+      pnpm: "pnpm add zustand",
+      yarn: "yarn add zustand",
+      bun: "bun add zustand",
+    }
+  },
+  {
+    NombreComando: "React Hook Form",
+    descripcion:
+      "React Hook Form es una biblioteca para manejar formularios en React. Proporciona una API simple y flexible para la validación y gestión del estado de los formularios.",
+    tamaño: "Variable",
+    categoria: CategoriaComando.UTILIDADES,
+    documentacion: "https://react-hook-form.com/get-started",
+    codigo: {
+      npm: "npm install react-hook-form",
+      pnpm: "pnpm add react-hook-form",
+      yarn: "yarn add react-hook-form",
+      bun: "bun add react-hook-form",
+    }
+  },
+   {
+    NombreComando: "React Router",
+    descripcion:
+      "React Router es una biblioteca para manejar la navegación en aplicaciones React. Proporciona una API declarativa para definir rutas y gestionar la navegación entre diferentes vistas.",
+    tamaño: "26.4 KB",
+    categoria: CategoriaComando.UTILIDADES,
+    documentacion: "https://reactrouter.com/home",
+    codigo: {
+      npm: "npm install react-router-dom",
+      pnpm: "pnpm add react-router-dom",
+      yarn: "yarn add react-router-dom",
+      bun: "bun add react-router-dom",
+    }
+  },
+    {
+    NombreComando: "TanStack Query",
+    descripcion:
+      "TanStack Query es una biblioteca para manejar la sincronización y el almacenamiento en caché de datos en aplicaciones React. Proporciona una API declarativa para definir consultas y gestionar el estado de los datos.",
+    tamaño: "26.4 KB",
+    categoria: CategoriaComando.ESTADO_DATOS,
+    documentacion: "https://tanstack.com/query/latest/docs/framework/react/installation",
+    codigo: {
+      npm: "npm install @tanstack/react-query",
+      pnpm: "pnpm add @tanstack/react-query",
+      yarn: "yarn add @tanstack/react-query",
+      bun: "bun add @tanstack/react-query",
+    }
+  },
+      {
+    NombreComando: "ECharts for React",
+    descripcion:
+      "ECharts for React es una biblioteca para crear gráficos interactivos en aplicaciones React. Proporciona una API simple y flexible para integrar gráficos de ECharts en componentes de React.",
+    tamaño: "2.2KB",
+    categoria: CategoriaComando.GRAFICOS,
+    documentacion: "https://git.hust.cc/echarts-for-react/",
+    codigo: {
+      npm: "npm install echarts-for-react",
+      pnpm: "pnpm add echarts-for-react",
+      yarn: "yarn add echarts echarts-for-react",
+      bun: "bun add echarts-for-react",
+    }
+  },
+
+
 
 
 
