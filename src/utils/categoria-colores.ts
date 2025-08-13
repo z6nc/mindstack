12e309d272075a43d93d12b13c601a7f73bdca80
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-13 17:57:23 -0500

    Add GRAFICOS category to CategoriaColores for improved color mapping

--- src/utils/categoria-colores.ts
+++ src/utils/categoria-colores.ts
@@ -4,7 +4,9 @@ export const CategoriaColores: Record<CategoriaComando, string> = {
   [CategoriaComando.INSTALACION]: "bg-blue-500/70 text-white border border-blue-500",
   [CategoriaComando.UI_ESTILOS]: "bg-purple-500/70 text-white border border-purple-500",
   [CategoriaComando.ANIMACIONES]: "bg-green-500/70 text-white border border-green-500",
+  [CategoriaComando.GRAFICOS]: "bg-orange-500/70 text-white border border-orange-500",
   [CategoriaComando.ESTADO_DATOS]: "bg-teal-500/70 text-white border border-teal-500",
   [CategoriaComando.UTILIDADES]: "bg-yellow-500/70 text-white border border-yellow-500",
   [CategoriaComando.TESTING_HERRAMIENTAS]: "bg-red-500/70 text-white border border-red-500",
+  
 };
